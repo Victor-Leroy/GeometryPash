9f15e96943225ede3046fa9bf1950b438c6d5e01
Maxime THIZEAU <maxime.thizeau@algosup.com>
2024-03-21 12:29:11 +0100

    Collision library

---
+++ Game/collisionLib.h
@@ -0,0 +1,25 @@
+#ifndef COLLISION
+#define COLLISION
+
+#include <SFML/Graphics.hpp>
+#include <cmath> // For std::abs and std::fmod
+#include <vector>
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For Time()
+#include <algorithm> // For std::remove_if
+#include<vector>
+
+using namespace std;
+
+class Obstacle{
+    double size;
+    double xPos;
+    double yPos;
+public:
+    vector get_position(){
+        vector<Obstacle> = xPos;
+        vector<Obstacle> = yPos;
+    }
+};
+
+#endif
